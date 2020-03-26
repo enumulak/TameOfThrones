5a6e85590e7730152de082fe301fa0f8c7c748d9
enumulak <enumula.kartik@outlook.com>
2020-03-26 19:02:42 +0530

    Added Seasar Tests

--- TameOfThrones.Tests/SeasarTest.cs
+++ TameOfThrones.Tests/SeasarTest.cs
@@ -8,10 +8,36 @@ namespace TameOfThrones.Tests
     [TestClass]
     public class SeasarTest
     {
+        //Method to test if Seasar is encrypting messages correctly
         [TestMethod]
-        public void TestSeasar()
+        public void TestSeasarEncryption()
         {
+            //Arrange
+            var encryptedMessage = "ABC"; //This is our expected value - For this test, we move '3' letters forward for encryption
+            var inputMessage = "xyz"; //This is the message that will be encrypted and will produce the above result
 
+            //Act
+            Seasar seasar = new Seasar();
+            string output = seasar.MessageHandler(inputMessage, 3, Seasar.CipherType.encryption);
+
+            //Assert
+            Assert.AreEqual(output, encryptedMessage);
+        }
+
+        //Method to test if Seasar is decrypting messages correctly
+        [TestMethod]
+        public void TestSeasarDecryption()
+        {
+            //Arrange
+            var decryptedMessage = "XYZ"; //This is our expected value - For this test, we move '3' letters backward for decryption
+            var inputMessage = "abc"; //This is the message that will be decrypted and will produce the above result
+
+            //Act
+            Seasar seasar = new Seasar();
+            string output = seasar.MessageHandler(inputMessage, 3, Seasar.CipherType.decryption);
+
+            //Assert
+            Assert.AreEqual(output, decryptedMessage);
         }
     }
 }
