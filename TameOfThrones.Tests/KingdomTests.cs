59900cb6ace7b0e48fe601eaf20b8902facd6556
enumulak <enumula.kartik@outlook.com>
2020-04-13 06:39:33 +0530

    Added Kingdom Tests

---
+++ TameOfThrones.Tests/KingdomTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TameOfThrones.Tests
+{
+    [TestClass]
+    public class KingdomTests
+    {
+        //This Method tests the following logic - A Kingdom becomes an Ally when it recieves a decrypted Message that contains its Symbol !!
+        //This Test is expected to return a True - Message sent by KingdomOne contains the Emblem of KingdomTwo, and so KingdomTwo becomes an Ally of KingdomOne
+        [TestMethod]
+        public void KingdomBecomesAlly()
+        {
+            //Arrange
+            
+            var kingdomOne = new Kingdom("Space", "Gorilla");
+            var kingdomTwo = new Kingdom("Air", "Owl");
+
+            //This message will be sent to the 'Air' kingdom and is encrypted. Our core logic will decrypt the message and that 
+            //message will contain characters from the symbol of 'Air'
+            var message = "rozo";
+
+            string ally = "AIR";
+
+            //Act
+
+            //Kingdom One sends the encrypted message to Kingdom Two
+            kingdomOne.SendMessage(message, kingdomTwo);
+            
+            //We obtain the Ally name from the Allies List of Kingdom One
+            string sAlly = kingdomOne.GetAllyName(0);
+            
+
+            //Assert
+            Assert.AreEqual(ally, sAlly);
+        }
+    }
+}
